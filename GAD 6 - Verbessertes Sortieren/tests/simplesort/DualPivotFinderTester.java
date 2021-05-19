26953cdb4ee7ca3438b92647d64dd64fcf45a3cb
Crsi <crsi@hopfen.space>
2021-05-19 21:56:01 +0200

    Created raw test structure for GAD 6

---
+++ GAD 6 - Verbessertes Sortieren/tests/simplesort/DualPivotFinderTester.java
@@ -0,0 +1,30 @@
+package tests.simplesort;
+
+import gad.simplesort.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DualPivotFinderTester {
+
+    @BeforeEach
+    void printLine() {
+        System.out.println("-------------------------");
+    }
+
+    @Test
+    void firstLastPivot() {
+    }
+
+    @Test
+    void randomPivot() {
+    }
+
+    @Test
+    void medianPivotFront() {
+    }
+
+    @Test
+    void medianPivotDistributed() {
+    }
+}
