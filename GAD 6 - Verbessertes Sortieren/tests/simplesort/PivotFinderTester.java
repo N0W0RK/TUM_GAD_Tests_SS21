03f4f6d3357915a9a6c3d7e9da791299c47c5d30
Jim M. R. Teichgräber <jimmr.teichgraeber@tum.de>
2021-05-21 10:22:48 +0200

    Add Tests for boundaries

--- GAD 6 - Verbessertes Sortieren/tests/simplesort/PivotFinderTester.java
+++ GAD 6 - Verbessertes Sortieren/tests/simplesort/PivotFinderTester.java
@@ -31,4 +31,39 @@ class PivotFinderTester {
     @Test
     void medianPivotDistributed() {
     }
+    
+    @Test
+    void testAll(){
+        bigArraysTestHelper(PivotFinder.getMidPivot(),300,30);
+        bigArraysTestHelper(PivotFinder.getRandomPivot(50),300,30);
+        bigArraysTestHelper(PivotFinder.getMedianPivotFront(3),300,30);
+        bigArraysTestHelper(PivotFinder.getMedianPivotFront(5),300,30);
+        bigArraysTestHelper(PivotFinder.getMedianPivotDistributed(3),300,30);
+        bigArraysTestHelper(PivotFinder.getMedianPivotDistributed(5),300,30);
+    }
+    
+    /**
+        Beware: This test helper is in some regards *stricter* than the Artemis tests. Good for finding exceptions and general edge cases regardless.
+        maxArrayLength 30 is recommended for general testing, 3 for the small array tests.
+        
+        Provided by the man, the myth, the legend: Ralg
+    */
+   static void bigArraysTestHelper(PivotFinder f, int seed, int maxArrayLength) {
+        Random ralg = new Random(seed);
+        for (int i = 0; i < 10000; i++) {
+            final int[] a = IntStream.generate(() -> ralg.nextInt(200) - 100).limit(i % maxArrayLength + 1).toArray();
+            int from = ralg.nextInt(a.length);
+            int to = ralg.nextInt(a.length);
+            if (from == to) continue;
+            if (to < from) {
+                int temp = to;
+                to = from;
+                from = temp;
+            }
+            final int result = f.findPivot(a, from, to);
+            assertTrue(result >= from && result <= to);
+            System.out.println("Array: " + Arrays.toString(a) + "\tfrom " + from + " to " + to + ":\t"
+                    + result);
+        }
+    }
 }
