6cefe126e79ab5237379926b9be7637cff0dac7b
Aamin Gem <gem.aamin@gmail.com>
2021-05-26 15:36:25 +0200

    Fixed the feedback from the last commit Added FastLoggingResult.java Added multiple tests for QuickSort with a single pivot but without SelectionSort optimisations

---
+++ GAD 6 - Verbessertes Sortieren/tests/simplesort/FastLoggingResult.java
@@ -0,0 +1,109 @@
+package tests.simplesort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Fast logging class which doesn't print anything to the console
+ * However stores all the would've been outputs in a list
+ * Abbreviations list
+ * SS = SelectionSort
+ * MS = MergeSort
+ * QS = QuickSort
+ * DPQS = DualPivotQuickSort
+ * JS = JavaSort
+ * LPA = LoggingPartialArray
+ * @author Aamin
+ */
+public class FastLoggingResult implements Result {
+
+    private final List<String> outputs = new ArrayList<>();
+
+    /**
+     * To log selection sort
+     * SS = SelectionSort
+     * @param array The array to be logged
+     * @param from The index from which it'll be logged (inclusive)
+     * @param to The index up to which it'll be logged (inclusive)
+     * @author Aamin
+     */
+    @Override
+    public void startSelectionsort(int[] array, int from, int to) {
+        outputs.add(("\nSS : " + Arrays.toString(array) + " in [" + from + ", " + to + "]"));
+    }
+
+    /**
+     * To log merge sort
+     * MS = MergeSort
+     * @param array The array to be logged
+     * @param from The index from which it'll be logged (inclusive)
+     * @param to The index up to which it'll be logged (inclusive)
+     * @author Aamin
+     */
+    @Override
+    public void startMergesort(int[] array, int from, int to) {
+        outputs.add("\nMS : " + Arrays.toString(array) + " in [" + from + ", " + to + "]");
+    }
+
+    /**
+     * To log quick sort
+     * QS = QuickSort
+     * @param array The array to be logged
+     * @param from The index from which it'll be logged (inclusive)
+     * @param to The index up to which it'll be logged (inclusive)
+     * @author Aamin
+     */
+    @Override
+    public void startQuicksort(int[] array, int from, int to) {
+        outputs.add("\nQS : " + Arrays.toString(array) + " in [" + from + ", " + to + "]");
+    }
+
+    /**
+     * To log DualPivotQuickSort
+     * DQPS = DualPivotQuickSort
+     * @param array The array to be logged
+     * @param from The index from which it'll be logged (inclusive)
+     * @param to The index up to which it'll be logged (inclusive)
+     * @author Aamin
+     */
+    @Override
+    public void startDualPivotQuicksort(int[] array, int from, int to) {
+        outputs.add("\nDPQS : " + Arrays.toString(array) + " in [" + from + ", " + to + "]");
+    }
+
+    /**
+     * To log java sort
+     * JS = JavaSort
+     * @param array The array to be logged
+     * @param from The index from which it'll be logged (inclusive)
+     * @param to The index up to which it'll be logged (inclusive)
+     * @author Aamin
+     */
+    @Override
+    public void startJavaSort(int[] array, int from, int to) {
+        outputs.add("\nJS : " + Arrays.toString(array) + " in [" + from + ", " + to + "]");
+    }
+
+    /**
+     * To log a partial array
+     * LPA = LoggingPartialArray
+     * @param array The array to be logged
+     * @param from The index from which it'll be logged (inclusive)
+     * @param to The index up to which it'll be logged (inclusive)
+     * @author Aamin
+     */
+    @Override
+    public void logPartialArray(int[] array, int from, int to) {
+        outputs.add("\n\tLPA : " + Arrays.toString(array) + " in [" + from + ", " + to + "]");
+    }
+
+    /**
+     * To be used at the end of the sorting to compare with the expected sort
+     * @return List of String
+     * @author Aamin
+     */
+    public List<String> getOutputs() {
+        return outputs;
+    }
+}
